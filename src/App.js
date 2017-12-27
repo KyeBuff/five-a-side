aeff46587e202490eb0dde1e83c70884fb6a6032
KyeBuff <kye.buffery@btinternet.com>
2017-12-27 14:13:39 +0000

    Added Jura font, landing content withe links

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import './App.min.css';
 import Header from './components/Header';
+import Landing from './components/landing-page/Landing';
+import {
+    Route,
+    Switch,
+} from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="container">
         <Header />
+        <Landing />
       </div>
     );
   }
---
+++ src/components/landing-page/Landing.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+    Link,
+} from "react-router-dom";
+
+const Landing = () => (
+	<section className="landing">
+		<h2 className="landing__heading">Build your 5 a-side team</h2>
+		<Link className="btn btn--progress" to="/new">Get started...</Link>
+		<p className="landing__text">Saved teams</p>
+		<Link className="btn" to="/">Team one vs Team two</Link>
+	</section>
+)
+
+export default Landing;
