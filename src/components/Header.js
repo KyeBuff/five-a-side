d98536a2eee681e0bc6fb5b690e6a81fcc4686ae
KyeBuff <kye.buffery@btinternet.com>
2017-12-27 13:25:38 +0000

    styled header component, added bg img

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 const Header = () => (
 	<header className="header">
-		<h1 className="header__main-heading">5 &lt;aside&gt;</h1>
+		<h1 className="header__main-heading">
+			5<br />
+			&lt;
+			<span>aside</span>
+			&gt;
+		</h1>
 	</header>
 )
 
